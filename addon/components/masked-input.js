0119d33d324241bca737c10b565892499ca663df
Kyle Dayton <kyle@kohactive.com>
2017-08-30 15:03:00 -0500

    Add additional config options

--- addon/components/masked-input.js
+++ addon/components/masked-input.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.TextField.extend({
     type: 'text',
     mask: null,
+    autoclear: true,
+    placeholderCharacter: "_",
 
     didReceiveAttrs() {
         this._super(...arguments);
@@ -18,7 +20,10 @@ export default Ember.TextField.extend({
     },
     didInsertElement: function() {
         var self = this;
-        this.$().mask(this.get('mask'));
+        this.$().mask(this.get('mask'), { 
+            autoclear: this.get('autoclear'),
+            placeholder: this.get('placeholderCharacter')
+        });
         this.$().keypress(function (event) { self.validate('13,48,49,50,51,52,53,54,55,56,57,45', event); });
     }
 });
--- addon/components/phone-number.js
+++ addon/components/phone-number.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import MaskedInput from 'ember-cli-masked-inputs/components/masked-input';
 
-export default Ember.TextField.extend({
+export default MaskedInput.extend({
     type: 'tel',
     mask: '999-999-9999',
 
